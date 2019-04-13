c1f72099671e8b09fe4a0ff1372d63ad98b7482f
[navindu] <[navindushane@gmail.com]>
2019-04-13 21:39:57 +0530

    template added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { CorouselComponent } from './components/corousel/corousel.component';
+import { RoomsComponent } from './components/rooms/rooms.component';
+import { AuthComponent } from './components/auth/auth.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    CorouselComponent,
+    RoomsComponent,
+    AuthComponent,
+
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/corousel/corousel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-corousel',
+  templateUrl: './corousel.component.html',
+  styleUrls: ['./corousel.component.css']
+})
+export class CorouselComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
