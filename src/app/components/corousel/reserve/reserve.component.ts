676a204c5348f4c44185441165d6460a99948832
navinduJay <navindushane@gmail.com>
2019-05-27 19:39:38 +0530

    room list update

--- src/app/components/corousel/reserve/reserve.component.ts
+++ src/app/components/corousel/reserve/reserve.component.ts
@@ -1,35 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-reserve',
-  templateUrl: './reserve.component.html',
-  styleUrls: ['./reserve.component.css']
+  selector: "app-reserve",
+  templateUrl: "./reserve.component.html",
+  styleUrls: ["./reserve.component.css"]
 })
 export class ReserveComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-
-    // $(".checkIn").datepicker({
-    //   format: "yyyy-mm-dd",
-    //   todayBtn: true,
-    //   autoclose: true,
-    //   startDate: new Date()
-    // })
-    // .on("changeDate", function(e) {
-    //   var checkInDate = e.date, $checkOut = $(".checkOut");
-    //   checkInDate.setDate(checkInDate.getDate() + 1);
-    //   $checkOut.datepicker("setStartDate", checkInDate);
-    //   $checkOut.datepicker("setDate", checkInDate).focus();
-    // });
-
-    // $(".checkOut").datepicker({
-    // format: "yyyy-mm-dd",
-    // todayBtn: true,
-    // autoclose: true
-    // });
-
-  }
-
+  ngOnInit() {}
 }
