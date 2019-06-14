9521928f24806dbec8ba7e16d5e8cc6d125b627a
navinduJay <navindushane@gmail.com>
2019-06-14 16:21:25 +0530

    lil updates

--- src/app/components/cus-payment/cus-payment.component.ts
+++ src/app/components/cus-payment/cus-payment.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import swal from "sweetalert";
 @Component({
-  selector: 'app-cus-payment',
-  templateUrl: './cus-payment.component.html',
-  styleUrls: ['./cus-payment.component.css']
+  selector: "app-cus-payment",
+  templateUrl: "./cus-payment.component.html",
+  styleUrls: ["./cus-payment.component.css"]
 })
 export class CusPaymentComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-  }
+    document
+      .getElementById("paymentMake")
+      .addEventListener("click", makePayment);
 
+    function makePayment() {
+      swal("Booking Success!", "", "success");
+    }
+  }
 }
